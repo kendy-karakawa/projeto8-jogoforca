35553d5f01074356a5a2f7f38be0d2caf1fcdf80
kendy-karakawa <kendy.karakawa@gmail.com>
2022-11-28 20:58:33 -0300

    Import de CSS e imagens no Javascript ok

--- src/App.js
+++ src/App.js
@@ -4,16 +4,24 @@ import Letras from "./componentes/Letras";
 import palavras from "./palavras";
 import alfabeto from "./componentes/Alfabeto";
 import { useState } from "react";
+import forca0 from "./assets/forca0.png"
+import forca1 from "./assets/forca1.png"
+import forca2 from "./assets/forca2.png"
+import forca3 from "./assets/forca3.png"
+import forca4 from "./assets/forca4.png"
+import forca5 from "./assets/forca5.png"
+import forca6 from "./assets/forca6.png"
+
 
 function App() {
   const imagens = [
-    "./assets/forca0.png",
-    "./assets/forca1.png",
-    "./assets/forca2.png",
-    "./assets/forca3.png",
-    "./assets/forca4.png",
-    "./assets/forca5.png",
-    "./assets/forca6.png",
+    forca0,
+    forca1,
+    forca2,
+    forca3,
+    forca4,
+    forca5,
+    forca6
   ];
 
   const [resposta, setResposta] = useState("");
